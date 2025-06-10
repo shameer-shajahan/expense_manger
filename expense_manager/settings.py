8e2922763b525de792cb12a6f0daa14982aa9ef0
shameer shajahan <shameershajahan17@gmail.com>
2025-06-10 13:40:56 +0530

    Update settings.py

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -145,5 +145,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'financetracker108@gmail.com'
-EMAIL_HOST_PASSWORD = 'scmv emut xgma msjm'  
